df774463fcf40233f0abed016805839907b8f638
Mayuri Raja <mraja7@utexas.edu>
2019-04-17 20:03:53 -0500

    fixed enums to be 0-indexed

--- src/enums.py
+++ src/enums.py
@@ -1,11 +1,12 @@
 from enum import Enum
 
 class EnergySource(Enum):
-    SOLAR = 1
-    WIND = 2
-    HYDRO = 3
+    SOLAR = 0
+    WIND = 1
+    HYDRO = 2
+    COAL = 3
 
 class WeatherConditions(Enum):
-    SUNLIGHT = 1
-    WIND = 2
-    PRECIPITATION = 3
+    SUNLIGHT = 0
+    WIND = 1
+    PRECIPITATION = 2
