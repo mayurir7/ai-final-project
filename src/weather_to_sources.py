9915929b2a67c679e4692e4d55d37da2fcdcddce
Mayuri Raja <mraja7@utexas.edu>
2019-05-01 14:34:39 -0500

    removed qlearner from weather to sources

--- src/weather_to_sources.py
+++ src/weather_to_sources.py
@@ -84,45 +84,6 @@ class FeatureExtractor():
         """
         return None
 
-
-class ApproximateQLearner():
-    """
-    self.weights: list storing the weights, index matches up with features
-    """
-    
-    def __init__(self):
-        self.weights = [0 for _ in range(len(WeatherConditions))]
-        self.featExtractor = FeatureExtractor()
-
-    def getWeights(self):
-        return self.weights
-
-    def getQValue(self, state, action):
-        """
-        Should return Q(state,action) = w * featureVector
-        where * is the dotProduct operator
-        """
-        featureVector = getFeatures()
-        weight = self.getWeights()
-        result = weight * featureVector  #matrix multiply?
-        return result
-
-    def update(self, state, action, nextState, reward):
-        """
-        Should update your weights based on transition
-        """
-        featureVector = self.featExtractor.getFeatures(state,action)
-        difference = reward + (self.discount * self.computeValueFromQValues(nextState)) - self.getQValue(state,action)
-        for feature in featureVector:
-            self.weights[feature] = self.getWeights()[feature] + (self.alpha * difference * featureVector[feature])
-
-    def calculateReward(self, state):
-        """
-        Calculates the reward for the given state
-        Reward should be a mix of balanced-ness of energy levels + 
-        """
-
-
 if __name__ == '__main__':
     test = FeatureExtractor()
 
