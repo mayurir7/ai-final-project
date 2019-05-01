d46c617def5c34376c05ff70f9f4f3bcdfcee88b
Mayuri Raja <mraja7@utexas.edu>
2019-05-01 14:53:51 -0500

    added missing computeValuesFromQValues method

--- src/energy_to_weather.py
+++ src/energy_to_weather.py
@@ -2,51 +2,55 @@
 #Wind: day’s conditions -  units of wind energy produced
 
 #state representation
-#[(solar, actual kWh?, predicted kWh?), (wind, kWh)...]
+#[[solar level, wind level, hydro level], day, hour]
+
+#action representation
+#int matching up with action enum
 
 #reward function
 #Given current state, action to be taken, next state, return value
 #How close the kWh it generated (prediction) was to the actual data 
 
+from energy_to_sources import State
+from energy_to_sources import FeatureExtractor
+from enums import EnergySource
+
 class ApproximateQAgent():  #(PacmanQAgent):
-                """
-                         ApproximateQLearningAgent
+    def __init__(self):
+        self.weights = [0 for _ in range(len(WeatherConditions))]
+        self.featExtractor = FeatureExtractor()
 
-                         You should only have to overwrite getQValue
-                         and update.  All other QLearningAgent functions
-                         should work as is.
-                """
-                
-                #def __init__(self, extractor='IdentityExtractor', **args):
-                def __init__(self):
-                                #self.featExtractor = util.lookup(extractor, globals())()
-                                #self.featExtractor = list of features!!
-                                #PacmanQAgent.__init__(self, **args)
-                                #self.weights = util.Counter()
-                                self.weights = [0 for _ in range(10)] #a list of the same length of features
+    def getWeights(self):
+        return self.weights
 
-                def getWeights(self):
-                                return self.weights
+    def getQValue(self, state, action):
+    """
+        Should return Q(state,action) = w * featureVector
+        where * is the dotProduct operator
+    """
+        featureVector = self.featExtractor.getFeatures(state, action)
+        weight = self.getWeights()
+        result = weight * featureVector
+        return result
 
-                def getQValue(self, state, action):
-                                """
-                                        Should return Q(state,action) = w * featureVector
-                                        where * is the dotProduct operator
-                                """
-                                "*** YOUR CODE HERE ***"
-                                featureVector = self.featExtractor.getFeatures(state, action)
-                                weight = self.getWeights()
-                                result = weight * featureVector
-                                return result
+    def computeValueFromQValues(self, state):
+    """
+        Returns Q value that comes from taking optimal action
+        in this state
+    """
+        maxVal = 0
+        for action in range(EnergySource):
+            temp = getQValue(state, action)
+            if temp > maxVal:
+                maxVal = temp
+        return maxVal
 
-                def update(self, state, action, nextState, reward):
-                                """
-                                         Should update your weights based on transition
-                                """
-                                "*** YOUR CODE HERE ***"
-                                featureVector = self.featExtractor.getFeatures(state,action)
-                                difference = reward + (self.discount * self.computeValueFromQValues(nextState)) - self.getQValue(state,action)
+    def update(self, state, action, nextState, reward):
+    """
+        Should update your weights based on transition
+    """
+        featureVector = self.featExtractor.getFeatures(state,action)
+        difference = reward + (self.discount * self.computeValueFromQValues(nextState)) - self.getQValue(state,action)
 
-                                for feature in featureVector:
-                                        self.weights[feature] = self.getWeights()[feature] + (self.alpha * difference * featureVector[feature])
-
+        for feature in featureVector:
+            self.weights[feature] = self.getWeights()[feature] + (self.alpha * difference * featureVector[feature])
