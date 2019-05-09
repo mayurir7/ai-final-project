7a31b9c016cc5276c6a4fd116e434ed8bc4e1159
brahma.seshu <brahma.seshu@gmail.com>
2019-05-08 19:39:49 -0500

    ran metrics

--- src/predict_sources.py
+++ src/predict_sources.py
@@ -16,7 +16,7 @@ class PredictSources():
     def __init__(self, path_to_data=None, path_to_energy=None):
         self.final_weights = self.getWeights()
         self.runner = Runner(0, 70000, 0, 0.1, 0.5, path_to_data, path_to_energy, debug=True)
-        self.capacity = list(self.runner.features.capacity) #capacity
+        self.capacity = list(self.runner.features.capacity)
         self.result = []
 
     def getWeights(self):
@@ -40,13 +40,11 @@ class PredictSources():
         """
 
         for index in range(len(self.runner.features.raw_data) - 1):
-            print"-----------------"
-            print "ITERATION: ", index
             raw_data, energy_gained, action, energy_levels, energy_needed = self.runner.predict_iterate()
             self.result.append((self.runner.features.raw_data[index], energy_gained, action, energy_levels, energy_needed))
 
 if __name__ == '__main__':
-    test = PredictSources(path_to_data = "../data/january.txt", path_to_energy="../data/2018load.csv")
+    test = PredictSources(path_to_data = "../data/april.txt", path_to_energy="../data/2018load.csv")
     test.prediction()
     total_energy_levels = 0.0
     total_renewables_used = 0.0
@@ -66,26 +64,6 @@ if __name__ == '__main__':
             total_energy_levels += energy
         total_energy_needed += energy_needed
         print actions, energy_levels, energy_needed
-
-    print("total_renewables_used", total_renewables_used)
-    print("total_energy_levels", total_energy_levels)
-    print("total_energy_needed", total_energy_needed)
-    print("total_coal_used", total_coal_used)
-
-    print("renewable utilization", total_renewables_used / total_energy_levels)
-
-# april:
-# ('total_renewables_used', 11188551.0)
-# ('total_energy_levels', 35319030.79544331)
-# ('total_energy_needed', 24830406.23999998)
-# ('total_coal_used', 13641855.240000002)
-
-#january
-# ('total_renewables_used', 9456142.0)
-# ('total_energy_levels', 147463509.14363718)
-# ('total_energy_needed', 30366945.820000038)
-# ('total_coal_used', 20910803.820000008)
-# ('renewable utilization', 0.06412530160793355)
 
 
 
